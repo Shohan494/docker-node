fed08fa4b6a009e7274f2d2c4745be2320a89ae7
Shohan494 <shohan12224943@gmail.com>
2021-01-03 16:25:50 +0600

    added console.log to index.js

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hi Hello');
+  res.send('How are you doing');
+});
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080');
 });
